2656f94a80408edb82ae9374c544c8b5e293d462
rokka <rokka@LAPTOP-678OGQJ2>
2020-12-03 08:51:46 +0530

    First commit

---
+++ CoachingClass/src/test/java/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="features/register.feature"
+		,glue="stepDefinitions"
+		,plugin = {"html:target/Destination"}
+			)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
